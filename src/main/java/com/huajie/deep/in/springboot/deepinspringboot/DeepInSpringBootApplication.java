714cde43cf720a3f97670923c40bc47d28422b33
Administrator <420842659@qq.com>
2020-05-07 17:55:19 +0800

    Initial commit

---
+++ src/main/java/com/huajie/deep/in/springboot/deepinspringboot/DeepInSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.huajie.deep.in.springboot.deepinspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeepInSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeepInSpringBootApplication.class, args);
+	}
+
+}
