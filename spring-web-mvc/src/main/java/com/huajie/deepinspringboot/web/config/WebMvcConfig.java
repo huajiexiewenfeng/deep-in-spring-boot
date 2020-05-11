35723a824760200fdd1ccd413ad0d9c103bba2f3
xwf <420842659@qq.com>
2020-05-11 22:35:43 +0800

    Spring Web mvc

---
+++ spring-web-mvc/src/main/java/com/huajie/deepinspringboot/web/config/WebMvcConfig.java
@@ -0,0 +1,37 @@
+package com.huajie.deepinspringboot.web.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.HandlerAdapter;
+import org.springframework.web.servlet.HandlerMapping;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter;
+import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ * Spring WebMvc 配置类
+ * 替代 app-context.xml
+ */
+@Configuration
+public class WebMvcConfig {
+    @Bean
+    public HandlerMapping handlerMapping(){
+        return new RequestMappingHandlerMapping();
+    }
+
+    @Bean
+    public HandlerAdapter handlerAdapter(){
+        return new RequestMappingHandlerAdapter();
+    }
+
+    @Bean
+    public ViewResolver viewResolver(){
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+}
