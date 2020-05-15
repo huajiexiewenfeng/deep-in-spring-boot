05f3e9aab6e44b9e32353224694854c6768d9573
Administrator <420842659@qq.com>
2020-05-15 14:13:42 +0800

    REST 内容协商流程

---
+++ springboot-rest/src/main/java/com/huajie/deepinspringboot/web/controller/UserRestController.java
@@ -0,0 +1,16 @@
+package com.huajie.deepinspringboot.web.controller;
+
+import com.huajie.deepinspringboot.web.domain.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserRestController {
+
+    @PostMapping("/echo/user")
+    public User user(@RequestBody User user) {
+        return user;
+    }
+
+}
